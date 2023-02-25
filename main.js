6e94bb1789b045d1d0d762a370ae1809c752d490
Evgeniy1109 <q126p@yandex.ru>
2023-02-25 20:14:04 +0300

    25.02 Изменения

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-
-
 document.querySelector('.burger').addEventListener('click',function(){
     document.querySelector('.burger span').classList.toggle('active');
     document.querySelector('.menu').classList.toggle('animate');
 });
+function menu_img_close() {
+    document.querySelector('.menu').classList.toggle('animate');
+}
 
+function show() {
+    document.querySelector('.modal_form_active').classList.toggle('modal_close')
+}
